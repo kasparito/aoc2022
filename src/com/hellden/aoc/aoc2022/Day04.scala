6dfd8e0befdcf8c9befb0b7b776f160bfda747b0
kaspar <kaspar@recordedfuture.com>
2022-12-04 12:04:14 +0100

    day 4 improved

--- src/com/hellden/aoc/aoc2022/Day04.scala
+++ src/com/hellden/aoc/aoc2022/Day04.scala
@@ -6,18 +6,16 @@ object Day04 extends Day(4):
 
   private val Pair = """(\d+)-(\d+),(\d+)-(\d+)""".r
 
+  private def parsePair: String => (Set[Int], Set[Int]) =
+    case Pair(a, b, c, d) =>
+      val e1 = a.toInt to b.toInt
+      val e2 = c.toInt to d.toInt
+      (e1.toSet, e2.toSet)
+
+  private val pairs = inputLines.map(parsePair)
+
   override def part1: Int = // 509
-    inputLines.count {
-      case Pair(a, b, c, d) =>
-        val e1 = (a.toInt to b.toInt).toSet
-        val e2 = (c.toInt to d.toInt).toSet
-        e1.subsetOf(e2) || e2.subsetOf(e1)
-    }
+    pairs.count { (e1, e2) => e1.subsetOf(e2) || e2.subsetOf(e1) }
 
   override def part2: Int = // 870
-    inputLines.count {
-      case Pair(a, b, c, d) =>
-        val e1 = (a.toInt to b.toInt).toSet
-        val e2 = (c.toInt to d.toInt).toSet
-        e1.intersect(e2).nonEmpty
-    }
+    pairs.count { (e1, e2) => e1.intersect(e2).nonEmpty }
