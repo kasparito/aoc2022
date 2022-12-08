0b7bd1d2a04a0b479ba3184ec5da3bab9cd6993d
kaspar <kaspar@recordedfuture.com>
2022-12-08 10:41:07 +0100

    day 8

---
+++ src/com/hellden/aoc/aoc2022/Day08.scala
@@ -0,0 +1,82 @@
+package com.hellden.aoc.aoc2022
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+object Day08 extends Day(8):
+
+  case class Position(x: Int, y: Int)
+
+  enum Direction(val dx: Int, val dy: Int):
+    case Up extends Direction(0, -1)
+    case Down extends Direction(0, 1)
+    case Left extends Direction(-1, 0)
+    case Right extends Direction(1, 0)
+
+    def move(position: Position): Position =
+      Position(position.x + dx, position.y + dy)
+
+  class Map[T](grid: IndexedSeq[IndexedSeq[T]]):
+    val horizontalBounds: Range = 0 until inputLines.map(_.length).max
+    val verticalBounds: Range = inputLines.indices
+
+    def move(position: Position, direction: Direction): Option[Position] =
+      val x = position.x + direction.dx
+      val y = position.y + direction.dy
+      Option.when(horizontalBounds.contains(x) && verticalBounds.contains(y)) {
+        Position(x, y)
+      }
+
+    def valueFor(position: Position): T =
+      grid(position.y)(position.x)
+
+  object Forest:
+    val map = new Map(inputLines.map(_.map(_.toString.toInt).toIndexedSeq).toIndexedSeq)
+
+    def positions: Iterable[Position] =
+      for
+        x <- map.horizontalBounds
+        y <- map.verticalBounds
+      yield
+        Position(x, y)
+
+    def visible(position: Position): Boolean =
+      val height = map.valueFor(position)
+
+      @tailrec
+      def visible(position: Position, direction: Direction): Boolean =
+        map.move(position, direction) match
+          case None =>
+            true
+          case Some(nextPosition) =>
+            map.valueFor(nextPosition) < height && visible(nextPosition, direction)
+
+      visible(position, Direction.Up) ||
+        visible(position, Direction.Down) ||
+        visible(position, Direction.Left) ||
+        visible(position, Direction.Right)
+
+    def scenicScore(position: Position): Long =
+      val height = map.valueFor(position)
+
+      @tailrec
+      def score(position: Position, direction: Direction, acc: Long = 0): Long =
+        map.move(position, direction) match
+          case None =>
+            acc
+          case Some(nextPosition) =>
+            if map.valueFor(nextPosition) >= height then
+              acc + 1
+            else
+              score(nextPosition, direction, acc + 1)
+
+      score(position, Direction.Up) *
+        score(position, Direction.Down) *
+        score(position, Direction.Left) *
+        score(position, Direction.Right)
+
+  override def part1: Long = // 1688
+    Forest.positions.count(Forest.visible)
+
+  override def part2: Long = // ???
+    Forest.positions.map(Forest.scenicScore).max
