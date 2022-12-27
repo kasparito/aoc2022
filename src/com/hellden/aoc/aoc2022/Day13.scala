dad77cb45bf26b2c3ebbe150ca8439d6cf4c3e30
kaspar <kaspar@recordedfuture.com>
2022-12-27 14:09:38 +0100

    day 13 simplified

--- src/com/hellden/aoc/aoc2022/Day13.scala
+++ src/com/hellden/aoc/aoc2022/Day13.scala
@@ -7,7 +7,6 @@ import scala.collection.immutable.SortedSet
 import scala.math.Ordered.orderingToOrdered
 import scala.util.parsing.combinator.*
 
-
 object Day13 extends Day(13):
   import com.hellden.aoc.aoc2022.Day13.Packet.Element
 
@@ -20,32 +19,18 @@ object Day13 extends Day(13):
     def element: Parser[Element] = number | packet
     def packet: Parser[Packet] = "[" ~ repsep(element, ",") ~ "]" ^^ { case _ ~ elements ~ _ => Packet(elements) }
 
-    def parse(s: String): Packet =
-      parse(packet, s) match
-        case Success(result, _) => result
-        case Failure(message, _) => throw new IllegalArgumentException(s"Invalid input: $s, $message")
+    def parse(s: String): Packet = parse(packet, s).get
 
-  private implicit object ElementOrdering extends Ordering[Element] {
+  given Ordering[Element] with
     override def compare(e1: Element, e2: Element): Int = (e1, e2) match
       case (i1: Int, i2: Int) => i1 compare i2
       case (i1: Int, p2: Packet) => compare(Packet(Seq(i1)), p2)
       case (p1: Packet, i2: Int) => compare(p1, Packet(Seq(i2)))
-      case (p1: Packet, p2: Packet) =>
-        val i1 = p1.elements.iterator
-        val i2 = p2.elements.iterator
-        while (i1.hasNext && i2.hasNext) {
-          val result = compare(i1.next(), i2.next())
-          if (result != 0) return result
-        }
-        i1.hasNext compare i2.hasNext
-  }
+      case (p1: Packet, p2: Packet) => p1.elements compare p2.elements
 
   override def part1: Int = // 5720
-    inputLines.toList.grouped(3).zipWithIndex.map {
-      case (packet1 :: packet2 :: _, index) if Packet.parse(packet1) <= Packet.parse(packet2) =>
-        index + 1
-      case _ =>
-        0
+    inputLines.toList.grouped(3).zipWithIndex.collect {
+      case (p1 :: p2 :: _, index) if Packet.parse(p1) <= Packet.parse(p2) => index + 1
     }.sum
 
   override def part2: Int = // 23504
